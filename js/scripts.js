d29089dd5b34cdccb02aefc33aa16029c83eda81
randeelayosa <randeelayosa@gmail.com>
2019-01-25 13:04:57 -0800

    Add split method to string of numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-var start = 0
+var start = 0;
+var number1 = "1";
+var number2 = "2";
+var number3 = "3";
 
 function range(start,end) {
   var rangeNumbers = [];
@@ -10,7 +13,17 @@ function range(start,end) {
 console.log(range(start,5));
 
   var numbers = range(start,5);
-  console.log(numbers)
+    console.log(numbers)
+  var numString = numbers.toString();
+    console.log(numString)
+  var numSplit = numString.split("");
+    console.log(numSplit)
+
+  numString.map(function(number1) {
+    return number1.replace(/[1]/gi, "Beep!");
+  });
+
+
 
 function string() {
   var str = rangeNumbers.toString();
